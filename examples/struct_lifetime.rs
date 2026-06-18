7c550ed84632de45e6c33479c31d6276cc911299
shigedangao <marc.inthaamnouay@gmail.com>
2026-06-18 22:46:44 +0200

    update struct_lifetime example

--- examples/struct_lifetime.rs
+++ examples/struct_lifetime.rs
@@ -4,6 +4,11 @@ fn transform_data<'a>(data: &'a [u8]) -> String {
     String::from_utf8_lossy(data).to_string()
 }
 
+fn transform_data_with_error<'a>(data: &'a [u8]) -> Result<&'a [u8], std::io::Error> {
+    // todo do something with data
+    Ok(data.iter().as_slice())
+}
+
 #[derive(Clone)]
 struct Input<'a> {
     data: &'a [u8],
@@ -15,8 +20,9 @@ struct Output {
     data: String,
 }
 
-#[bricke(converter = "From", source = "Input", lifetimes = 'a)]
+#[bricke(converter = "TryFrom", source = "Input", try_error_type = "std::io::Error", lifetimes = 'a)]
 struct OutputLifetime<'a> {
+    #[bricke_field(transform_fn = "transform_data_with_error", is_fallible = true)]
     data: &'a [u8],
 }
 
@@ -24,8 +30,8 @@ fn main() {
     let input = Input { data: b"hello" };
     let output: Output = Output::from(input.clone());
 
-    let output_lifetime = OutputLifetime::from(input);
+    let output_lifetime = OutputLifetime::try_from(input);
 
     dbg!(output.data);
-    dbg!(output_lifetime.data);
+    assert!(output_lifetime.is_ok());
 }
